68fb7352e8434b4c88bd33a3eddf356cc8b6bd3c
Caleb Braaten <caleb.braaten@gmail.com>
2015-02-07 15:14:28 -0800

    Revert "Added Piston Code"
    This reverts commit 2e79c0b4387d76553a54748fb2bf471833d68e2f.

--- Mecanum Drive/src/Robot.cpp
+++ Mecanum Drive/src/Robot.cpp
@@ -1,6 +1,9 @@
 #include "WPILib.h"
 
-class Robot: public RobotCode
+/**
+ * This is a demo program showing how to use Mecanum control with the RobotDrive class.
+ */
+class Robot: public SampleRobot
 {
 
     // Channels for the wheels
@@ -8,23 +11,23 @@ class Robot: public RobotCode
     const static int rearLeftChannel	= 3;
     const static int frontRightChannel	= 1;
     const static int rearRightChannel	= 0;
+
     const static int joystickChannel	= 0;
+    //Compressor comp;
+    //DoubleSolenoid Piston;
 	RobotDrive robotDrive;	// robot drive system
-	Joystick stick;
+	Joystick stick = new Joystick(1,5,8);			// only joystick
+	bool trigger = stick.(1);
 	float Xaxis = stick.GetX();
 	float Yaxis = stick.GetY();
 	float Zaxis = stick.GetZ();
-	DoubleSolenoid Piston;
-	int POV = stick.GetPOV();
-	const int button1 = 1;
-	
 
 public:
 	Robot() :
-			Piston(1,2),
+			//Piston(1,2),
 			robotDrive(frontLeftChannel, rearLeftChannel,
 					   frontRightChannel, rearRightChannel),	// these must be initialized in the same order
-			stick(joystickChannel)							// as they are declared above.
+			stick(0)							// as they are declared above.
 	{
 		robotDrive.SetExpiration(0.1);
 		robotDrive.SetInvertedMotor(RobotDrive::kFrontLeftMotor, true);	// invert the left side motors
@@ -36,19 +39,17 @@ public:
 	 */
 	void OperatorControl()
 	{
+		//compressor.Start();//Starts the compressor
 		robotDrive.SetSafetyEnabled(false);
 		while (IsOperatorControl() && IsEnabled())
 		{
-			if(stick.GetRawButton[(button1)] == true)//I doubt this [(button1)] thing will work, but it got eclipse to shut up about my errors
-			{
-				Piston.Set(DoubleSolenoid::kForward);
+        	// Use the joystick X axis for lateral movement, Y axis for forward movement, and Z axis for rotation.
+        	// This sample does not use field-oriented drive, so the gyro input is set to zero.
+			if (trigger == true){
+				robotDrive.MecanumDrive_Cartesian(stick.GetX()/5, stick.GetY()/5, stick.GetZ()/5.5);
+			}else if(trigger == false){
+				robotDrive.MecanumDrive_Cartesian(stick.GetX()/2, stick.GetY()/2, stick.GetZ()/2.5);//Moves the motors when the stick moves
 			}
-			else if(stick.GetRawButton[(button1)] == false)//I doubt this [(button1)] thing will work, but it got eclipse to shut up about my errors
-			{
-				Piston.Set(DoubleSolenoid::kReverse);
-			}
-
-			robotDrive.MecanumDrive_Cartesian(stick.GetX()/2, stick.GetY()/2, stick.GetZ()/2.5);//Moves the motors when the stick moves
 			Wait(0.005); // wait 5ms to avoid hogging CPU cycles
 		}
 	}
